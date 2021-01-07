d5af09e3f27374675665e864eef3aa0c2e17375b
Tatsunosuke Shimada <adam.is.t521@gmail.com>
2021-01-08 01:14:01 +0900

    ver up

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.helloWorld = void 0;
 exports.helloWorld = function () {
-    return 'Howdy!';
+    return 'Howdy! YO!';
 };
 //# sourceMappingURL=index.js.map
